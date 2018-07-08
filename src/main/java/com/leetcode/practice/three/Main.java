2bbf3132d48f856dc960a68d5ecdd7bc50de13c6
Dragon <applynext@163.com>
2018-07-08 12:24:09 +0800

    增加了第1题与第2题的题目说明

---
+++ src/main/java/com/leetcode/practice/three/Main.java
@@ -0,0 +1,11 @@
+package com.leetcode.practice.three;
+
+public class Main {
+    public static void main(String[] args) {
+        String testStr1 = new String("abcabcbb");
+        String testStr2 = new String("bbbbb");
+        String testStr3 = new String("pwwkew");
+
+
+    }
+}
